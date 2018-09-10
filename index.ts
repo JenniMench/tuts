b2014c7bbc5a29cca34fe8626385c83470020612
JenniMench <jennilyn@inspireteam.com>
2018-09-10 17:06:43 +0800

    develop commit for tuts2

--- index.ts
+++ index.ts
@@ -5,12 +5,13 @@ async function init() {
         await command('git fetch --all');
         const { stdout: branchName } = await command('git rev-parse --abbrev-ref HEAD');
         await command('git add .');
-        await command('git commit -m "initial develop commit for tuts2"');
+        await command('git commit -m "develop commit for tuts2"');
 
         await command('git checkout develop');
         await command('git pull --rebase');
         await command(`git checkout ${branchName}`);
         await command('git rebase develop');
+        await command('git push --force-with-lease');
     } catch(e) {
         console.log('error!:', e);
     }
